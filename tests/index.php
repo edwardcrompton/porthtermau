141c5f1c474dfd65b6d229984ed0f8f3b693278e
Edward Crompton <edward.crompton@gmail.com>
2020-11-17 20:22:41 +0000

    Add documentation to rudimentary test

--- tests/index.php
+++ tests/index.php
@@ -1,9 +1,11 @@
 <?php
-
+/**
+ * Usage:
+ * PORTHTERMAU_API_KEY='KEY_HERE' php tests/index.php
+ */
 include('src/PorthTermau/PorthTermauWrapper.php');
 
 $api = new PorthTermau\PorthTermauWrapper(['referer' => 'http://llennatur.cymru']);
 
 echo $api->translateTerm('en', 'Vulpes vulpes') . "\n";
 echo $api->translateTerm('cy', 'Vulpes vulpes') . "\n";
-
